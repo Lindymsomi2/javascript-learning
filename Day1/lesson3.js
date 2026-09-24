fdc0263b106e9c6a75448f160297d6512015ff2a
Lindymsomi2 <lindymsomi2@gmail.com>
2026-09-24 14:36:19 +0200

    objects

--- Day1/lesson3.js
+++ Day1/lesson3.js
@@ -1,4 +1,5 @@
 const marksArray = [25, 55, 66, 90, 95, 48, 77, 76, 99, 59, 88];
+const marksArray2 = [45, 75, 86, 90, 95, 28, 76, 76, 99, 67, 88];
 
 console.log(marksArray);
 
@@ -18,13 +19,20 @@ marksArray.shift(); //removed a mark at the start of the array, can use pop() to
 console.log(marksArray);
 
 let sum = 0;
+let sum2 = 0;
 
 marksArray.forEach((mark) => {
   sum = sum + mark;
 });
 console.log(sum);
+//second array
+marksArray2.forEach((mark) => {
+  sum2 = sum2 + mark;
+});
+console.log(sum2);
 
 const average = sum / marksArray.length;
+const average2 = sum2 / marksArray2.length;
 console.log(average);
 console.log(Math.round(average)); //rounded off
 
@@ -40,6 +48,7 @@ if (average >= 70) {
 
 //loops practice
 //check every mark and report whether pass or fail
+
 for (let i = 0; i <= marksArray.length - 1; i++) {
   if (marksArray[i] >= 50) {
     console.log("Pass");
@@ -82,6 +91,17 @@ for (let i = 0; i <= marksArray.length - 1; i++) {
   }
 }
 console.log(`highest is ${highest}`);
+//hightest for 2nd array
+let highest2 = 0;
+
+for (let i = 0; i <= marksArray2.length - 1; i++) {
+  let firstElement = marksArray2[i];
+
+  if (firstElement > highest2) {
+    highest2 = firstElement;
+  }
+}
+console.log(`highest is ${highest2}`);
 
 //lowest mark in the array
 let lowest = highest;
@@ -94,3 +114,94 @@ for (let i = 0; i <= marksArray.length - 1; i++) {
   }
 }
 console.log(`lowest is ${lowest}`);
+
+//lowest mark in the 2nd array
+let lowest2 = highest2;
+
+for (let i = 0; i <= marksArray2.length - 1; i++) {
+  let firstElement = marksArray2[i];
+
+  if (firstElement < lowest2) {
+    lowest2 = firstElement;
+  }
+}
+console.log(`lowest is ${lowest2}`);
+
+//lesson 4, objects
+
+const student = {
+  name: "Melissa",
+  marks: marksArray,
+  average: average,
+  highest: highest,
+  lowest: lowest,
+};
+
+console.log(student.name);
+console.log(student.average);
+console.log(student.highest);
+console.log(student.lowest);
+
+function studentGrade(average) {
+  if (average >= 90 && average <= 100) {
+    const result = "Grade A";
+    console.log(result);
+    return result;
+  } else if (average >= 70) {
+    const result = "Grade B";
+    console.log(result);
+    return result;
+  } else if (average >= 60) {
+    const result = "Grade C";
+    console.log(result);
+    return result;
+  } else if (average >= 50) {
+    const result = "Grade D";
+    console.log(result);
+    return result;
+  } else {
+    const result = "Fail";
+    console.log(result);
+    return result;
+  }
+}
+
+student.grade = studentGrade(student.average);
+
+console.log(student);
+student.name = "Larry";
+
+student.grade = studentGrade(student.average);
+
+function passed(average) {
+  if (average >= 50) {
+    const result = "Pass";
+    return result;
+  } else {
+    const result = "Fail";
+    return result;
+  }
+}
+
+student.pass = passed(student.average);
+
+console.log(student);
+
+const student2 = {
+  name: "Precious",
+  sum: sum2,
+  marks: marksArray2,
+  average: average2,
+  highest: highest2,
+  lowest: lowest2,
+};
+student2.grade = studentGrade(student2.average);
+student2.pass = passed(student2.average);
+console.log(student2);
+
+const reports = {
+  student1: student,
+  student2: student2,
+};
+
+console.log(reports);
